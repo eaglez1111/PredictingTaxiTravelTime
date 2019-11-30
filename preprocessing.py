9e1152d61b961c97ddb983b444d285b1d0e35031
George Cazenavette <gjc006@latech.edu>
2019-11-29 20:52:40 -0500

    working random forest regressor

--- preprocessing.py
+++ preprocessing.py
@@ -15,26 +15,34 @@ euclidianDistance /= np.max(euclidianDistance)
 travelDistance = np.load("FeatureData_processed/TravelDistance.npy", allow_pickle=True)
 travelDistance /= np.max(travelDistance)
 
-total_rows = 0
+t2secs = lambda t: 3600*t.hour + 60*t.minute + t.second
+dt2secs = lambda dt: dt.seconds + 86400*dt.days
+getHour = lambda t: t.hour
+
 for i in range(674):
-    print("{}/647".format(i+1))
+    print("{}/674".format(i+1))
     df = TaxiDataLoader([i])
+
+    # handles those weird rows
     filter = pd.Series(df['t0'].dt.year == 2017)
     df = df[filter.values]
-    total_rows += len(df)
+
+    df['start_time'] = df['t0'].apply(t2secs)
+    df['travel_time'] = df['dt'].apply(dt2secs)
     df['euc_dist'] = euclidianDistance[df['loc0'], df['loc1']]
     df['real_dist'] = travelDistance[df['loc0'], df['loc1']]
     df['bor0'] = zoneBorough[df['loc0']]
     df['bor1'] = zoneBorough[df['loc1']]
-    # print(df['t0'])
+
     hours = df['t0'].values.astype('datetime64[h]')
-    # print(hours)
-    df['hour'] = pd.to_datetime(hours).hour
-    hoursArray = np.asarray(df['hour'])
+
+    hoursArray = np.asarray(pd.to_datetime(hours).hour)
     df['morn'] = np.logical_and(hoursArray >= 4, hoursArray < 12).astype(int)
     df['aftnoon'] = np.logical_and(hoursArray >= 12, hoursArray < 20).astype(int)
     df['night'] = np.logical_or(hoursArray >= 20, hoursArray < 4).astype(int)
     # df['hour'] = df['hour'].astype(CategoricalDtype(categories=range(24)))
+    df['hour'] = df['t0'].apply(getHour)
+
     weatherLists = [weather[time] for time in hours]
     df['humidity'] = [row[0] for row in weatherLists]
     df['windspeed'] = [row[1] for row in weatherLists]
@@ -44,16 +52,17 @@ for i in range(674):
     df['fog'] = [row[6] for row in weatherLists]
     df['rain'] = [row[7] for row in weatherLists]
     df['snow'] = [row[8] for row in weatherLists]
+
     days = df['t0'].values.astype('datetime64[D]')
     df['hday'] = [d in holidays for d in days]
+
+    df['hour'] = df['hour'].astype(CategoricalDtype(categories=range(24)))
     df['wkday'] = df['wkday'].astype(CategoricalDtype(categories=range(7)))
-    df = pd.get_dummies(df, columns=['bor0', 'bor1'])
-    if i == 0:
-        df.to_csv('PreProcessedData/data.csv', header='column_names')
-    else:
-        df.to_csv('PreProcessedData/data.csv', mode='a', header=False)
+
+    df['bor0'] = df['bor0'].astype(CategoricalDtype(categories=range(6)))
+    df['bor1'] = df['bor1'].astype(CategoricalDtype(categories=range(6)))
+
+    df = pd.get_dummies(df, columns=['bor0', 'bor1', 'wkday', 'hour'])
+
     df.to_pickle('PreProcessedData/df_{}.pkl'.format(i+1))
 
-
-# print(weather)
-# print(len(weather[0].keys()))
